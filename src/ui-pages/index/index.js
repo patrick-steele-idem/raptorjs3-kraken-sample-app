c36ff1a45a42e0baa463ade6a64a0e6959c0bfd5
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-02-27 15:19:04 -0700

    Commented out meddle ware

--- src/ui-pages/index/index.js
+++ src/ui-pages/index/index.js
@@ -3,16 +3,28 @@ var express = require('express');
 var raptorTemplates = require('raptor-templates');
 var templatePath = require.resolve('./template.rhtml');
 
-var app = express();
+// var app = express();
 
-app.use(meddleware(require('./middleware.json')));
+// app.use(meddleware(require('./middleware.json')));
 
-app.use(function(req, res, next) {
+// app.use(function(req, res, next) {
+//     var viewData = {
+//         name: 'John',
+//         count: 30,
+//         time: Date.now()
+//     };
+//     raptorTemplates
+//         .stream(templatePath, viewData)
+//         .pipe(res);
+// });
+
+module.exports = function(req, res, next) {
     var viewData = {
         name: 'John',
-        count: 30
+        count: 30,
+        time: Date.now()
     };
-    raptorTemplates.stream(templatePath, viewData).pipe(res);
-});
-
-module.exports = app;
+    raptorTemplates
+        .stream(templatePath, viewData)
+        .pipe(res);
+};
