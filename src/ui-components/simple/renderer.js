659b01155968069a23a61408dac339a8bf3ebd5f
Patrick Steele-Idem <pnidem@gmail.com>
2014-03-07 11:06:55 -0700

    Moved tag def into renderer.js

--- src/ui-components/simple/renderer.js
+++ src/ui-components/simple/renderer.js
@@ -1,6 +1,14 @@
 var raptorTemplates = require('raptor-templates');
 var templatePath = require.resolve('./template.rhtml');
 
+exports.tag = {
+    attributes: {
+        name: 'string',
+        time: 'expression',
+        "*": 'string'
+    }
+};
+
 exports.render = function(input, context) {    
     raptorTemplates.render(templatePath, input, context);
 };
