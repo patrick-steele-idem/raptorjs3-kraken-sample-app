475dd617de1496ee0ffc36d46d191f1dee5026c1
Patrick Steele-Idem <pnidem@gmail.com>
2014-03-07 11:03:44 -0700

    Auto-discover tags

--- src/ui-components/foo/renderer.js
+++ src/ui-components/foo/renderer.js
@@ -1,8 +1,6 @@
 var raptorTemplates = require('raptor-templates');
 var templatePath = require.resolve('./template.rhtml');
 
-
-
 exports.render = function(input, context) {
     raptorTemplates.render(
         templatePath,
